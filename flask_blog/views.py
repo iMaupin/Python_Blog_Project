79be9f9b44ee2c01fdc22d42f812e7078b0d6fab
Isaac <Isaac.Maupin@gmail.com>
2022-06-17 07:38:51 -0400

    fixed issue in users_loader

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -12,8 +12,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
-
+    return User.query.get(user_id)
 
 def admin_only(function):
     @wraps(function)
